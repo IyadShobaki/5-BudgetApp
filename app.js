8ebcb1159013990859868ddcd2095839e0d4ecef
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-24 11:01:15 -0400

    Create an Initialization Function
    Adds init function inside controller to start the app.
    Called the init function in the main window (oustide the controller)

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 // Budget module/controller to process the data coming from he UI
-var budgetController = (function() {
-    
-    
+var budgetController = (function () {
+
+
 })();
 
 // UI module/controller to read the data from the UI
-var UIController = (function(){
+var UIController = (function () {
     var DOMstrings = {
-        inputType:'.add__type',
-        inputDescription:'.add__description',
+        inputType: '.add__type',
+        inputDescription: '.add__description',
         inputValue: '.add__value',
         inputBtn: '.add__btn'
     };
 
     return {
-        getInput: function(){
+        getInput: function () {
             return { // to return the three elements we should return them as an object
                 type: document.querySelector(DOMstrings.inputType).value, //Will be eaither inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
-            };          
+            };
         },
-        getDOMstrings: function(){
+        getDOMstrings: function () {
             return DOMstrings;
         }
     };
@@ -29,42 +29,52 @@ var UIController = (function(){
 })();
 
 // The global app controller to connect the budget and UI modules
-var controller = (function(budgetCtrl, UICtrl){
-    
-    var DOM = UICtrl.getDOMstrings();
+var controller = (function (budgetCtrl, UICtrl) {
 
-    var ctrlAddItem = function(){
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
+        // Add event listner to add button
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+        // Add key event to do the same thing (when the user click the add button)
+        // if the user press the enter key in the keyboard
+        document.addEventListener('keypress', function (event) {
+            //console.log(event); //the key code for the enter key is 13 
+            if (event.keyCode === 13 || event.which === 13) { //some older browser use which property
+                //console.log('Enter key was pressed.');
+                ctrlAddItem();
+            }
+        });
+    };
 
-       // 1. Get the field input data
-            var input = UICtrl.getInput();
-            console.log(input);
+    var ctrlAddItem = function () {
 
-       // 2. Add the item to the budget controller
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
 
-       // 3. Add the item to the UI
+        // 2. Add the item to the budget controller
 
-       // 4. Calculate the budget
+        // 3. Add the item to the UI
 
-       // 5. Display the budget on the UI
+        // 4. Calculate the budget
 
-
-    }
+        // 5. Display the budget on the UI
 
 
-    // Add event listner to add button
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
-    
-    // Add key event to do the same thing (when the user click the add button)
-    // if the user press the enter key in the keyboard
-    document.addEventListener('keypress', function(event) {
-        //console.log(event); //the key code for the enter key is 13 
-        if(event.keyCode === 13 || event.which === 13){ //some older browser use which property
-            //console.log('Enter key was pressed.');
-            ctrlAddItem();
-        } 
-    });
+    };
+
+    return {
+        init: function(){
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    };
+
+
+
 
 })(budgetController, UIController);
 
 
 
+// The only code outside controllers to start the app
+controller.init();
