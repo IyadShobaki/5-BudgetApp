fb33521b052153be39537a82315c2816e8f52efd
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-24 10:20:07 -0400

    Setting up the First Event Listeners
    Adds 'add button' and 'Enter key' event listeners

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
+// Budget module/controller to process the data coming from he UI
 var budgetController = (function() {
-    var x = 23;
-
-    var add = function(a){
-        return x + a;
-    }
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    }
+    
+    
 })();
 
+// UI module/controller to read the data from the UI
 var UIController = (function(){
     // Some code
 })();
 
+// The global app controller to connect the budget and UI modules
 var controller = (function(budgetCtrl, UICtrl){
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function(){
 
-    return {
-        anotherPublic: function(){
-            console.log(z);
-        }
+       // 1. Get the field input data
+
+       // 2. Add the item to the budget controller
+
+       // 3. Add the item to the UI
+
+       // 4. Calculate the budget
+
+       // 5. Display the budget on the UI
+
+       console.log('It works')
+
     }
 
+
+    // Add event listner to add button
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+    
+    // Add key event to do the same thing (when the user click the add button)
+    // if the user press the enter key in the keyboard
+    document.addEventListener('keypress', function(event) {
+        //console.log(event); //the key code for the enter key is 13 
+        if(event.keyCode === 13 || event.which === 13){ //some older browser use which property
+            //console.log('Enter key was pressed.');
+            ctrlAddItem();
+        } 
+    });
 
 })(budgetController, UIController);
 
