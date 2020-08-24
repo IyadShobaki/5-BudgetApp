9224d70f8485831b45dd1b151b1677b78ff0e0d8
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-24 16:59:14 -0400

    Clearing Input Fields in the UI
    - Added a function in UIController to clear the fields after submitting.
    - call clearFields function from the controller.

--- app.js
+++ app.js
@@ -94,6 +94,25 @@ var UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
         },
+        clearFields: function(){
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription 
+                + ', '+ DOMstrings.inputValue);
+            
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array){ //index, array are optional
+               current.value = "";
+            }); 
+
+            //fieldsArr.forEach(element => { // Iyad - another way
+                //element.value = "";
+            //});
+
+            fieldsArr[0].focus();
+
+        },
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -130,7 +149,10 @@ var controller = (function (budgetCtrl, UICtrl) {
 
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
-        
+
+        // 4. Clear the fields
+        UICtrl.clearFields();
+
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
